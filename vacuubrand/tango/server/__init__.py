f6d19dc4d3db50d93bf3805b57a2d558e3f01909
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-07-02 19:14:59 +0200

    tango: add missing main()

--- vacuubrand/tango/server/__init__.py
+++ vacuubrand/tango/server/__init__.py
@@ -1,1 +1,10 @@
-    
+from .dcp3000 import DCP3000
+
+
+def main():
+    import sys
+    import logging
+    from tango.server import run
+    args = ['Vacuubrand'] + sys.argv[1:]
+    logging.basicConfig(level="DEBUG")
+    run((DCP3000,), args=args)
