b29c1bd9eed4131b50918fb8d2aa82fd38cbaf47
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-07-02 12:34:42 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -5,33 +5,46 @@
 import sys
 from setuptools import setup, find_packages
 
-requirements = ['pyserial', 'PyTango']
+requirements = ["pyserial"]
+
+with open("README.md") as f:
+    description = f.read()
 
 
 setup(
-    name='vacuubrand',
+    name="vacuubrand",
     author="Tiago Coutinho",
-    author_email='tcoutinho@cells.es',
-    version='1.0.0',
+    author_email="tcoutinho@cells.es",
+    version="1.0.0",
     description="vacuubrand library",
-    long_description="Vacuubrand library (DCP 3000)",
+    long_description=description,
+    long_description_content_type="text/markdown",
     entry_points={
-        'console_scripts': [
-            'PyDsVacuuBrand = vacuubrand.tango.server:main',
+        "console_scripts": [
+            "Vacuubrand = vacuubrand.tango.server:main [tango]",
         ]
     },
+    extras_require={
+        "tango": ["pytango"],
+        "simulator": ["sinstruments>=1"]
+    },
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
     ],
     install_requires=requirements,
     license="LGPLv3",
     include_package_data=True,
-    keywords='vacuubrand, library, tango',
+    keywords="vacuubrand, library, tango",
     packages=find_packages(),
-    url='https://github.com/ALBA-Synchrotron/vacuubrand')
+    url="https://github.com/tiagocoutinho/vacuubrand",
+    project_urls={
+        "Documentation": "https://github.com/tiagocoutinho/vacuubrand",
+        "Source": "https://github.com/tiagocoutinho/vacuubrand"
+    }
+)
