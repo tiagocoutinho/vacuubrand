e27c6bd5abf854ce9ceac2b6953e5d2a6248940b
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-07-03 17:35:04 +0200

    Improve simulator

--- vacuubrand/simulator.py
+++ vacuubrand/simulator.py
@@ -22,7 +22,10 @@ from sinstruments.simulator import BaseDevice
 
 
 DEFAULT = {
-    'IN_PV_1': '1004.1 mbar',
+    "IN_VER": "DCP 3000  V2.30",
+    "IN_PV_S1": "1005.7 mbar",
+    "IN_ERR": "00000",
+
 }
 
 
@@ -31,13 +34,20 @@ class DCP3000(BaseDevice):
 
     def __init__(self, name, **opts):
         kwargs = {}
-        if 'newline' in opts:
-            kwargs['newline'] = opts.pop('newline')
+        if "newline" in opts:
+            kwargs["newline"] = opts.pop("newline")
         self._config = dict(DEFAULT, **opts)
+        self._on = True
         super().__init__(name, **kwargs)
 
     def handle_message(self, line):
-        self._log.debug('request %r', line)
+        self._log.debug("request %r", line)
         line = line.decode().strip()
-        return self._config[line].encode() + b'\r\n'
+        if line.startswith("REMOTE"):
+            self._on = line.rsplit(" ", 1)[-1] == "1"
+            return
+        elif line == 'IN_PV_1':
+            v = "1004.1" if self._on else "0000.0"
+            return v + " mbar\r\n"
+        return self._config[line].encode() + b"\r\n"
 
